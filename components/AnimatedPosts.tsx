1e0f2ab3cfb9ebcd2cb1a277404a79f263eb9ed2
anm7 <nsite2014@gmail.com>
2024-09-16 00:14:51 +0300

    style

--- components/AnimatedPosts.tsx
+++ components/AnimatedPosts.tsx
@@ -30,7 +30,7 @@ let title = props.title;
         // tl.fromTo(".post__title", {scale:0.5}, {scale:1, opacity:1});
         // tl.fromTo(".post__menu", {y:20}, {y:0, opacity:1});
         tl.fromTo(".post__body", {y:20}, {y:0, opacity:1});
-        // tl.fromTo(".hero__glow", {scale:0.5}, {scale:1, opacity:1});
+        tl.fromTo(".post__glow", {scale:0.5}, {scale:1, opacity:1});
 
 
     }, {scope:container})
@@ -45,37 +45,71 @@ let title = props.title;
                 <Spotlight className='top-10 left-90 h-[80vh] w-[50vw]' fill='purple' />
                 <Spotlight className='top-28 left-80 h-[80vh] w-[50vh]' fill='blue' />
             </div>
-             <h1 className='post__title page-title uppercase tracking-widest text-[1.5rem] text-center text-blue-100'>
-            {title}
-        </h1 >
+            <h1 className='post__title page-title uppercase tracking-widest text-[1.5rem] text-center text-blue-100'>
+                {title}
+            </h1 >
             <PostCatMenu />
-			<div className="posts post__body opacity-0">
-				{posts.map((post: any, idx: number) => {
-                    console.log("Post")
-                    console.log(post)
-					return (
-						<Card className='mb-[20px]' key={idx}>
-                            <Image 
-                                src={post.featuredImage.node.sourceUrl} 
-                                alt="image" 
-                                width={600} 
-                                height={400}
-                                className='rounded-t-lg object-cover'
-                            />
-                            <CardHeader className='py-[5px] my-[5px] blog-card-head'>
-                                <h3 className='text-center text-lg line-clamp-2'>{post.title}</h3>
-                            </CardHeader>
-                            <CardBody className='p-[5px]'>
-                                <p className='line-clamp-2 text-sm'>{post.shortDesc}</p>
-                            </CardBody>
-                            <CardFooter>
-                                <Button className='w-full mt7'>
-                                    <Link className='post-link' href={`${post.slug}`}>Виж</Link>
-                                </Button>
-                            </CardFooter>
-                        </Card>
-					);
-				})}
+            <div className="post__body opacity-0 post-wrap-dark p-[20px] mt-[20px]">
+                <div className="post__glow opacity-0 glow-post absolute -z-10 aspect-square w-full  bg-blue-400/20 blur-3xl filter" />
+                <div className="posts">
+                    {posts.map((post: any, idx: number) => {
+                        console.log("Post")
+                        console.log(post)
+                        return (
+                            <Card className='mb-[20px]' key={idx}>
+                                <Image 
+                                    src={post.featuredImage.node.sourceUrl} 
+                                    alt="image" 
+                                    width={600} 
+                                    height={400}
+                                    className='rounded-t-lg object-cover'
+                                />
+                                <CardHeader className='py-[5px] my-[5px] blog-card-head'>
+                                    <h3 className='text-center text-lg line-clamp-2'>{post.title}</h3>
+                                </CardHeader>
+                                <CardBody className='p-[5px]'>
+                                    <p className='line-clamp-2 text-sm'>{post.shortDesc}</p>
+                                </CardBody>
+                                <CardFooter>
+                                    <Button className='w-full mt7'>
+                                        <Link className='post-link' href={`${post.slug}`}>Виж</Link>
+                                    </Button>
+                                </CardFooter>
+                            </Card>
+                        );
+                    })}
+                </div>
+			</div>
+            <div className="post__body opacity-0 post-wrap p-[20px] mt-[20px]">
+                <div className="post__glow opacity-0 glow-home absolute -z-10 aspect-square w-full  bg-blue-400/20 blur-3xl filter" />
+                <div className="posts">
+                    {posts.map((post: any, idx: number) => {
+                        console.log("Post")
+                        console.log(post)
+                        return (
+                            <Card className='mb-[20px]' key={idx}>
+                                <Image 
+                                    src={post.featuredImage.node.sourceUrl} 
+                                    alt="image" 
+                                    width={600} 
+                                    height={400}
+                                    className='rounded-t-lg object-cover'
+                                />
+                                <CardHeader className='py-[5px] my-[5px] blog-card-head'>
+                                    <h3 className='text-center text-lg line-clamp-2'>{post.title}</h3>
+                                </CardHeader>
+                                <CardBody className='p-[5px]'>
+                                    <p className='line-clamp-2 text-sm'>{post.shortDesc}</p>
+                                </CardBody>
+                                <CardFooter>
+                                    <Button className='w-full mt7'>
+                                        <Link className='post-link' href={`${post.slug}`}>Виж</Link>
+                                    </Button>
+                                </CardFooter>
+                            </Card>
+                        );
+                    })}
+                </div>
 			</div>
         </div>
     )
